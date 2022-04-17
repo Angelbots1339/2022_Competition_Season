378325d7ad4fb2ca9c419bb5e08eceb5801e3488
DiegoP-S <dipe2559@colorado.edu>
2022-04-16 22:28:32 -0600

    net tables helper more functionality

--- src/main/java/frc/robot/utils/NetworkTablesHelper.java
+++ src/main/java/frc/robot/utils/NetworkTablesHelper.java
@@ -109,4 +109,68 @@ public class NetworkTablesHelper {
             .getEntry(entryName)
             .getDouble(defaultEntry);
     }
+
+    /**
+     * Sets a boolean from given table/entry
+     * @param tableName
+     * @param entryName
+     * @param entry
+     * @return False if the entry exists with a different type
+     */
+    public static boolean setBoolean(String tableName, String entryName, boolean entry) {
+        return NetworkTableInstance
+            .getDefault()
+            .getTable(tableName)
+            .getEntry(entryName)
+            .setBoolean(entry);
+    }
+
+    /**
+     * Sets a boolean from given table/subtable/entry
+     * @param tableName
+     * @param subTableName
+     * @param entryName
+     * @param entry
+     * @return False if the entry exists with a different type
+     */
+    public static boolean setBoolean(String tableName, String subTableName, String entryName, boolean entry) {
+        return NetworkTableInstance
+            .getDefault()
+            .getTable(tableName)
+            .getSubTable(subTableName)
+            .getEntry(entryName)
+            .setBoolean(entry);
+    }
+
+    /**
+     * Sets a double from given table/entry
+     * @param tableName
+     * @param entryName
+     * @param entry
+     * @return False if the entry exists with a different type
+     */
+    public static boolean setDouble(String tableName, String entryName, double entry) {
+        return NetworkTableInstance
+            .getDefault()
+            .getTable(tableName)
+            .getEntry(entryName)
+            .setDouble(entry);
+    }
+
+    /**
+     * Sets a double from given table/subtable/entry
+     * @param tableName
+     * @param subTableName
+     * @param entryName
+     * @param entry
+     * @return False if the entry exists with a different type
+     */
+    public static boolean setDouble(String tableName, String subTableName, String entryName, double entry) {
+        return NetworkTableInstance
+            .getDefault()
+            .getTable(tableName)
+            .getSubTable(subTableName)
+            .getEntry(entryName)
+            .setDouble(entry);
+    }
 }
